ba78144a2d1f3006e7d493bea386b96e527f6b41
Ryan Bagwell <ryan@ryanbagwell.com>
2013-04-09 11:02:52 -0500

    adding project layout

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Django Muckraking',
+    version='0.1',
+    author='Ryan Bagwell',
+    author_email='ryan@ryanbagwell.com',
+    url='https://github.com/ryanbagwell/django-muckraking',
+    packages=['muckraking',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description="A collection of random Django utilities",
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "Django >= 1.4",
+
+    ],
+)
