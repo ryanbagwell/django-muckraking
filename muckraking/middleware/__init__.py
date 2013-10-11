3971429cda8261d5fb9a8aa4b3a9d2c6682d66bd
Ryan Bagwell <ryan@ryanbagwell.com>
2013-10-11 17:10:27 -0500

    adding the legacy url redirect middleware

--- muckraking/middleware/__init__.py
+++ muckraking/middleware/__init__.py
@@ -1,3 +1,6 @@
+import re
+import os
+from django.http import HttpResponsePermanentRedirect
 from django.utils.html import strip_spaces_between_tags as short
 from django.conf import settings
 
@@ -11,3 +14,43 @@ class SpacelessMiddleware(object):
         if not settings.DEBUG and 'text/html' in response['Content-Type']:
             response.content = short(response.content)
         return response
+
+
+
+class LegacyURLRedirectMiddleware:
+
+    """ This middleware lets you match a specific url and redirect
+        the request to an new url.
+
+        You keep a tuple of url regex pattern/url redirect tuples on your site
+        settings.
+
+        For example, in settings.py:
+
+        LEGACY_URL_REDIRECTS = (
+            (r'^/about/news-events\.php(.+)?', '/news-events/$1',),
+            (r'^/about/(?!index\.php)', '/about/',),
+        )
+
+        """
+
+    def process_request(self, request):
+
+        path = request.META['PATH_INFO']
+
+        LEGACY_URL_REDIRECTS = getattr(settings, 'LEGACY_URL_REDIRECTS', None)
+
+        if LEGACY_URL_REDIRECTS is None: return
+
+        for url_pattern, redirect_url in LEGACY_URL_REDIRECTS:
+            regex = re.compile(url_pattern)
+            matched = regex.match(path)
+
+            if matched:
+                print matched.groups()
+
+                for i, group in enumerate(matched.groups(), start=1):
+                    print group
+                    redirect_url = redirect_url.replace("$%s" % str(i), group)
+
+                return HttpResponsePermanentRedirect(redirect_url)
