5433f7aa912f3f185f5dc6b473f8c668d5dfc661
Ryan Bagwell <ryan@ryanbagwell.com>
2013-11-21 16:58:41 -0600

    changing name, package find

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='Django Muckraking',
+    name='django-muckraking',
     version='0.2',
     author='Ryan Bagwell',
     author_email='ryan@ryanbagwell.com',
     url='https://github.com/ryanbagwell/django-muckraking',
-    packages=['muckraking',],
+    packages=find_packages(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description="A collection of random Django utilities",
     long_description=open('README.rst').read(),
